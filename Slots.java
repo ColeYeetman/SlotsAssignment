70ef8404e0bde88f130c7e9c5f7df7ed49ffa859
ColeYeetman <yeatmanco@limestone.on.ca>
2023-05-26 08:49:16 -0400

    Generate / Print
    Added method to generate the slot machine and print it out when wanted

--- Slots.java
+++ Slots.java
@@ -4,19 +4,29 @@ public class Slots {
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
         int spin = 10;
+        int spinDuration = 250;
+        int[][] machine = new int[9][3];
 
-        int[][] machine = {{1,1,1},
-                           {2,2,2},
-                           {3,3,3},
-                           {4,4,4},
-                           {5,5,5},
-                           {6,6,6},
-                           {7,7,7},
-                           {8,8,8},
-                           {9,9,9}};
+        generateMachine(machine);
+        printMachine(machine);
 
     }
-    public static void spin(int spin, int[][] machine, int spinLength){
-        
+    public static void spin(int spin, int[][] machine, int spinDuration){
+        int temp = 0;
+    }
+    public static void generateMachine(int[][] machine){
+        for (int row = 0; row < machine.length; row++){
+            for (int col = 0; col < machine[row].length; col++){
+                machine[row][col] = (int) (Math.random() * 9 + 1);
+            }
+        }
+    }
+    public static void printMachine(int[][] machine){
+        for (int i = 0; i < machine.length; i++){
+            for (int j = 0; j < machine[i].length; j++){
+                System.out.print(machine[i][j]);
+            }
+            System.out.print("\n");
+        }
     }
 }
