d632868d62d0b89d8304179c872a50f85c009e47
ColeYeetman <yeatmanco@limestone.on.ca>
2023-05-26 08:28:45 -0400

    Initial Commit
    This reverts commit 50e621762af3924e590a7d4931204703fbf4d094.

---
+++ Slots.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Slots {
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        int spin = 10;
+
+        int[][] machine = {{1,1,1},
+                           {2,2,2},
+                           {3,3,3},
+                           {4,4,4},
+                           {5,5,5},
+                           {6,6,6},
+                           {7,7,7},
+                           {8,8,8},
+                           {9,9,9}};
+
+    }
+    public static void spin(int spin, int[][] machine, int spinLength){
+        
+    }
+}
